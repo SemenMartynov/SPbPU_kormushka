9c0ce82e320dc5135484239317771f509372472a
Semen Martynov <semen.martynov@gmail.com>
2015-03-20 22:49:12 +0300

    Spell checks and migrations

--- venv/bin/kormushka/advert/models.py
+++ venv/bin/kormushka/advert/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Advert(models.Model):
-    class Meta:
-        db_table = "advert"
-    title = models.CharFiel(max_length = 200)
+    class Meta():
+        db_table = 'advert'
+    title = models.CharField(max_length = 200)
     text = models.TextField()
     date = models.DateTimeField()
-    likes = model.Integeg()
+    like = models.IntegerField()
