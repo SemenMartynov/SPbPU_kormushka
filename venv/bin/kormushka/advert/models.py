31f7f7ba28b5ac9c165d57f74e4aaa3fe929ed6c
Semen Martynov <semen.martynov@gmail.com>
2015-03-20 22:13:20 +0300

    Model for advert

--- venv/bin/kormushka/advert/models.py
+++ venv/bin/kormushka/advert/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Advert(models.Model):
+    title = models.CharFiel(max_length = 200)
+    text = models.TextField()
+    date = models.DateTimeField()
+    likes = model.Integeg()
