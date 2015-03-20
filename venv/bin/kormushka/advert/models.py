7162ae4faa1e70f873b288bc70b508f8e31b7fd0
Semen Martynov <semen.martynov@gmail.com>
2015-03-20 22:14:31 +0300

    Table name for advert set

--- venv/bin/kormushka/advert/models.py
+++ venv/bin/kormushka/advert/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Advert(models.Model):
+    class Meta:
+        db_table = "advert"
     title = models.CharFiel(max_length = 200)
     text = models.TextField()
     date = models.DateTimeField()
