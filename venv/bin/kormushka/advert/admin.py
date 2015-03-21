f62a3bf42c2b79f9bfe57f0ae9a5603c9436cc0e
Semen Martynov <semen.martynov@gmail.com>
2015-03-22 00:01:33 +0300

    Likes field hided from admin

--- venv/bin/kormushka/advert/admin.py
+++ venv/bin/kormushka/advert/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from advert.models import Advert, Comment
 
 # Register your models here.
-admin.site.register(Advert)
+class AdvertAdmin(admin.ModelAdmin):
+    fields = ['title', 'text', 'date']
+
+admin.site.register(Advert, AdvertAdmin)
