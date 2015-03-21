5600ead7960f8c7d617a37ae8742bf03cc900e2a
Semen Martynov <semen.martynov@gmail.com>
2015-03-22 00:05:58 +0300

    Comments after Advert

--- venv/bin/kormushka/advert/admin.py
+++ venv/bin/kormushka/advert/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from advert.models import Advert, Comment
 
 # Register your models here.
+class AdvertInline(admin.StackedInline):
+    model = Comment
+    extra = 3
+
 class AdvertAdmin(admin.ModelAdmin):
     fields = ['title', 'text', 'date']
+    inlines = [AdvertInline]
 
 admin.site.register(Advert, AdvertAdmin)
