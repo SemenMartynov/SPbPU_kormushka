01c97d85035f5739f7981618da04727a4a5b07a9
Semen Martynov <semen.martynov@gmail.com>
2015-03-23 22:58:19 +0300

    Cleanup - task #45

--- kormushka/kormushka01/kormushka01/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-
-urlpatterns = patterns('',
-    url(r'^webapp/', include('webapp.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-)
