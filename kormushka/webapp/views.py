c2d5a81dfbbd275f4d363115f14121778bddb58a
newmumba <abramov91@mail.ru>
2015-04-01 23:02:55 +0300

    #50.rework profile

--- kormushka/webapp/views.py
+++ kormushka/webapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib import auth
 from django.core.context_processors import csrf
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url="/login/")
 def index(request):
 
 	return render(request,'list.html', {
