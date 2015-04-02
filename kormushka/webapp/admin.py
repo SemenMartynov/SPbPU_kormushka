8b203808a4d90a20dc8c80fbbe1ec8f977398860
newmumba <abramov91@mail.ru>
2015-04-02 14:00:07 +0300

    add models

--- kormushka/webapp/admin.py
+++ kormushka/webapp/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from webapp import models
 
 admin.site.register(models.Purchase)
+admin.site.register(models.Category)
+admin.site.register(models.Depart)
+admin.site.register(models.POP)
+admin.site.register(models.PO)
--- kormushka/webapp/models.py
+++ kormushka/webapp/models.py
@@ -1,16 +1,48 @@
 from django.db import models
 from loginsys.models import CustomUser
 
+#Тип покупки
+class Category(models.Model):
+	name = models.CharField("Тип покупки", max_length = 30)
+
+	def __str__(self):
+		return self.name
+
+#Отделы
+class Depart(models.Model):
+	name = models.CharField("Название",max_length=100,unique=True)
+	depart = models.IntegerField("Состоит в",max_length=30,default = 0)
+
+	def __str__(self):
+		return self.name
+
 #Покупки
 class Purchase(models.Model):
 	name = models.CharField("Название",max_length=50)
 	user = models.ForeignKey(CustomUser, verbose_name="Пользователь")
-#	depart = models.ForeignKey(Depart,verbose_name = "Отдел")
+	depart = models.ForeignKey(Depart,verbose_name = "Отдел")
 	date = models.DateTimeField("Дата совершения",auto_now=True, blank=True)
 	cost = models.FloatField("Сумма",default = 0)
 	state = models.IntegerField("Состояние",default = 0)#оплачена или нет
 	about = models.TextField(blank=True)
-#	category = models.ForeignKey(Category)
+	category = models.ForeignKey(Category)
 
 	def __str__(self):
 		return self.name
+
+#ПОП - Покупатель - Отдел - Покупка
+class POP(models.Model):
+	user = models.ForeignKey(CustomUser, verbose_name="Пользователь")
+	depart = models.ForeignKey(Depart, verbose_name = "Отдел")
+	purchase = models.ForeignKey(Purchase, verbose_name = "Покупка")
+
+	def __str__(self):
+		return '%s %s (%s) - %s' % (self.user.last_name, self.user.first_name, self.depart.name,self.purchase.name)
+
+#ПО - Покупатель - Отдел
+class PO(models.Model):
+	user = models.ForeignKey(CustomUser, verbose_name = "Пользователь")
+	depart = models.ForeignKey(Depart, verbose_name = "Отдел")
+
+	def __str__(self):
+		return '%s %s (%s)' % (self.user.last_name, self.user.first_name, self.depart.name)
