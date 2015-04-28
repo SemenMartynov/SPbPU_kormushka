f3f8733280f087d0e91f2a0a2cacef5baeeb345b
newmumba <abramov91@mail.ru>
2015-04-28 20:16:58 +0300

    #51. fix form Purchase 2

---
+++ kormushka/webapp/static/webapp/js/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+   $('#button-add').click(function(){
+    alert('f');
+    var str = '<div>sdsfsdfsf</div>';
+    $('#block-partner').append(str);
+   })
+})
--- kormushka/webapp/views.py
+++ kormushka/webapp/views.py
@@ -13,18 +13,23 @@ def index(request):
 	purchase = Purchase.objects.filter(user=request.user.pk)
 	depart = PO.objects.filter(user=request.user.pk)
 	category = Category.objects.all()
-	args = {'purchase' : purchase,'title':'kormushka','user':auth.get_user(request),'category':category,'depart':depart}
+	args = {'purchase' : purchase,'title':'kormushka','user':auth.get_user(request),'category':category,'depart':depart, 'formadd':'collapse'}# formadd - форма добавления свернута
 	args.update(csrf(request))
 	return render(request,'list.html', args)
 
 @login_required(login_url="/login/")
 def addpurchase(request):
+	args = {}
+	args.update(csrf(request))
 	if request.POST:
 		form = PurchaseForm(request.POST, request.FILES)
 		if form.is_valid():
-			purchase = form.save(commit=False)
-			purchase.user = CustomUser.objects.get(id=auth.get_user(request).pk)
-			purchase.date = datetime.datetime.now()
-			purchase.state = 0
-			form.save()
+			#проверяем, состоит ли пользователь в указанной группе. Защищает от подмены value.
+			po = PO.objects.filter(user = request.user.pk, depart = request.POST['depart']) 
+			if po:
+				purchase = form.save(commit=False)
+				purchase.user = CustomUser.objects.get(id=auth.get_user(request).pk)
+				purchase.date = datetime.datetime.now()
+				purchase.state = 0
+				form.save()
 	return redirect('/')
