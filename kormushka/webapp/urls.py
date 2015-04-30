4c8ef08de277e1c83cac8608f9ebfb3e5de8a695
newmumba <abramov91@mail.ru>
2015-04-30 14:56:49 +0300

    add purchase members

--- kormushka/webapp/urls.py
+++ kormushka/webapp/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('webapp.views',
 	url(r'^$','index'),
 	url(r'^addpurchase/$','addpurchase'),
+	url(r'^get-users-by-name/$','getUsersByName'),
+	url(r'^get-purchase-users/$','getPurchaseUsers'),
 )
--- kormushka/webapp/views.py
+++ kormushka/webapp/views.py
@@ -2,15 +2,23 @@ from django.shortcuts import render, redirect
 from django.contrib import auth
 from django.core.context_processors import csrf
 from django.contrib.auth.decorators import login_required
-from webapp.models import Purchase,Depart,Category,PO
+from webapp.models import Purchase,Depart,Category,PO,POP
 from webapp.forms import PurchaseForm
 from loginsys.models import CustomUser
+from django.http import HttpResponse
 import datetime
 import json
+import logging
+from webapp.serializers import AllFieldsSerializer
 
 @login_required(login_url="/login/")
 def index(request):
-	purchase = Purchase.objects.filter(user=request.user.pk)
+	k=[]
+	l = POP.objects.filter(user=request.user.pk)
+	for i in l:
+	 	k.append(i.purchase.id)
+
+	purchase = Purchase.objects.filter(pk__in=k)
 	depart = PO.objects.filter(user=request.user.pk)
 	category = Category.objects.all()
 	args = {'purchase' : purchase,'title':'kormushka','user':auth.get_user(request),'category':category,'depart':depart, 'formadd':'collapse'}# formadd - форма добавления свернута
@@ -32,4 +40,37 @@ def addpurchase(request):
 				purchase.date = datetime.datetime.now()
 				purchase.state = 0
 				form.save()
+
+				lastPurchase =  Purchase.objects.latest('id').pk
+				userpk = request.POST['userpk'].split(",")
+				userpk = list(set(userpk))
+				for i in userpk:
+					party = POP(user=CustomUser.objects.get(id=i), purchase=Purchase.objects.get(id=lastPurchase), depart=Depart.objects.get(id=1))
+					party.save()
+
 	return redirect('/')
+
+def getUsersByName(request):
+	name = request.POST.get('name')
+	ser = AllFieldsSerializer()
+
+	all_objects = list(CustomUser.objects.filter(last_name__icontains=name))
+	l = list()
+	for obj in all_objects:
+		l.append({"label": obj.get_full_name(), "value": obj.pk})
+	return HttpResponse(json.dumps(l))
+
+def getPurchaseUsers(request):
+	purchaseId = request.POST.get('purchaseId')
+
+	l = POP.objects.filter(purchase=Purchase.objects.get(id=purchaseId))
+	k=[]
+	for i in l:
+	 	k.append(i.user.id)
+	logger = logging.getLogger(__name__)
+	logger.warn(k)
+	user = list(CustomUser.objects.filter(pk__in=k))
+	li=list()
+	for obj in user:
+		li.append({"label": obj.get_full_name(), "pk": obj.pk})
+	return HttpResponse(json.dumps(li))		
