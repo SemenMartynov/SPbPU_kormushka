ccf7b74d139999af394d3805005d5bb879fd96dc
dsv_git <dsv.mail@yandex.ru>
2015-05-05 06:41:11 +0300

    #54 add sidebar lighter when click and load users from first depart in list

--- kormushka/webapp/static/webapp/js/departs/view.js
+++ kormushka/webapp/static/webapp/js/departs/view.js
@@ -1,35 +1,60 @@
 $(document).ready(function () {
-
+    //  vars
     var departSidebarItems = $("#departs-sidebar .depart-sidebar-item");
     var usersDiv = $("#users-by-depart");
 
     var userTemplate = _.template("<li data-user-id='<%= id %>'><%= fullName %></li>");
     var userTemplateEmpty = _.template("В этом отделе пока еще нет сотрудников...");
 
-    departSidebarItems.click(function() {
-        var departId = $(this).attr("data-depart-id");
-
-        $.ajax({
+    //  functions
+    var getUsersByDepart = function(departId) {
+        return $.ajax({
             url: "/get-users/",
             type: "POST",
             dataType: "json",
             data: {
                 "csrfmiddlewaretoken" : csrf_token,
                 "departId": departId
-            },
-            success: function(data) {
-                if ($.isArray(data) && data.length!=0) {
-                    usersDiv.html("");
-                    _.each(data, function(v) {
-                        usersDiv.append(userTemplate(v));
-                    });
-                } else {
-                    usersDiv.html(userTemplateEmpty());
-                }
-            },
-            error: function(e) {
-                console.log(e);
             }
+        });
+    }
+
+    var fillUsersDiv = function(users) {
+        if ($.isArray(users) && users.length!=0) {
+            usersDiv.html("");
+            _.each(users, function(v) {
+                usersDiv.append(userTemplate(v));
+            });
+        } else {
+            usersDiv.html(userTemplateEmpty());
+        }
+    };
+
+    //  init view
+    departSidebarItems.removeClass("active");
+    departSidebarItems.first().parents("li").addClass("active");
+    var departId = departSidebarItems.first().attr("data-depart-id");
+    getUsersByDepart(departId)
+    .done(function(result) {
+        fillUsersDiv(result);
+    })
+    .fail(function(e) {
+        console.log(e);
+    });
+
+    //  listeners
+    departSidebarItems.click(function() {
+        departSidebarItems.parents("li").removeClass("active");
+        $(this).parents("li").addClass("active");
+
+        var departId = $(this).attr("data-depart-id");
+
+        getUsersByDepart(departId)
+        .done(function(result) {
+            fillUsersDiv(result);
+        })
+        .fail(function(e) {
+            console.log(e);
         });
     });
 
